93b93ff237c7be26991ae1ab295119d846c6b0a2
Trevor F. Smith <trevor@trevor.smith.name>
2017-11-07 10:27:51 -0800

    Add a readme and switch to static HTML instead of pug templates.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Social graffiti prototype server
+
+This repo contains an Express HTTPD that implements the bare bones API to host the social graffiti demo using WebXR.
+
+While it may, at first glace, appear to be a production-ready anchor name service or a 3D content service, it is neither.
+
+*This is a prototype built quickly for a single demo.*
+
+## Running
+
+	cd social-graffiti
+	npm install
+	DEBUG=social-graffiti:* npm start
+
--- app.js
+++ app.js
@@ -5,25 +5,40 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// We use static HTML from /public instead of pug for everything except the error page
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Serve static files other than HTML
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// API routes
 app.use('/users', users);
+
+// Serve static HTML
+var options = {
+  dotfiles: 'ignore',
+  etag: true,
+  extensions: ['html'],
+  index: 'index.html',
+  lastModified: true,
+  maxAge: '1d',
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+    res.header('Cache-Control', 'public, max-age=1d');
+  }
+};
+app.use('/', express.static(path.join(__dirname, 'public'), options));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
